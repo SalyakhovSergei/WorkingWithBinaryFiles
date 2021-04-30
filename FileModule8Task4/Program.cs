67226eeee1ce3094478be1008c313d8859131b47
Sergey <ssm1988@inbox.ru>
2021-04-30 16:47:41 +1000

    Добавьте файлы проекта.

---
+++ FileModule8Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FileModule8Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ReadFile();
+ 
+        }
+    }
+}
---
+++ FileModule8Task4/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileModule8Task4
+{
+    class Student
+    {
+        string name;
+        
+
+        public string path = "C:/Users/Анатолий/Desktop/Students.dat";
+
+        public void ReadFile()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    fs.Position = 0x123;
+                    name = br.ReadString();
+
+                }
+                Console.WriteLine(name);
+            }
+            
+        }
+    }
+}
